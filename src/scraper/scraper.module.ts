d3fe8fbfab098b2c6435e7ce3bbb87970232cdfc
Steravy <steph.74lw@gmail.com>
2024-04-10 18:31:57 -0100

    chore: test log in in life miles.

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ScraperController } from './scraper.controller';
 import { ScraperFilteredProductService } from './services/scraper.filtered-product.service';
 import { ScraperProductDetailsService } from './services/scraper.product-details.service';
+import { LifeMilesService } from './services/life-miles.service';
 
 @Module({
     controllers: [ScraperController],
-    providers: [ScraperFilteredProductService, ScraperProductDetailsService],
+    providers: [
+        ScraperFilteredProductService,
+        ScraperProductDetailsService,
+        LifeMilesService,
+    ],
 })
-export class ScraperModule {}
+export class ScraperModule implements OnModuleInit {
+    constructor(
+      private readonly lifeMiles: LifeMilesService
+    ) {
+    }
+
+   async onModuleInit(): Promise<void> {
+       await this.lifeMiles.run();
+    }
+}
---
+++ src/scraper/services/life-miles.service.ts
@@ -0,0 +1,80 @@
+/* 
+    Created by Stefan Vitoria on 2/23/24
+*/
+
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { composeUrl } from '../utils/utils';
+import puppeteer, { Page } from 'puppeteer';
+import { ProductDetailsHtmlParser } from '../utils/product-details-html-parser';
+
+@Injectable()
+export class LifeMilesService {
+    private readonly LOGGER_LABEL = LifeMilesService.name;
+
+    async run() {
+        const url =
+            'https://oauth.lifemiles.com/login?login_challenge=1c655e2ccf8b4790a3c286abf7dee436';
+        const browser = await puppeteer.launch({headless: false});
+        Logger.log('BROWSER LAUNCHED', this.LOGGER_LABEL);
+        try {
+            const currentPage = await browser.newPage();
+            currentPage.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+            // NAVIGATE TO LOGINPAGE
+            await this.navigateToLoginPage(currentPage);
+
+            // await currentPage.goto(url);
+            Logger.log('SMILES LOGIN WEB PAGE ACCESSED', this.LOGGER_LABEL);
+
+            await currentPage.waitForSelector('#username', { visible: true });
+            await currentPage.type('#username', '00024567815', { delay: 100 });
+            await currentPage.waitForSelector('#password', { visible: true });
+            await currentPage.type('#password', 'PassWord@24', { delay: 100 });
+
+            await Promise.all([
+                currentPage.waitForNavigation({ waitUntil: 'load' }),
+                currentPage.click('#Login-confirm'),
+            ]);
+
+
+
+            // currentPage.on('request', (request) => {
+            //     if (
+            //         request.url() ===
+            //         'https://oauth.lifemiles.com/authentication/token/grant'
+            //     ) {
+            //         console.log(request.url(), 'REQUEST URL');
+            //     } else request.continue();
+            // });
+        } catch (e) {
+            if (e instanceof BadRequestException) throw e;
+            Logger.error(
+                'ERROR WHILE LOGGING',
+                this.LOGGER_LABEL,
+            );
+            console.log(e);
+        } finally {
+            if (browser) await browser.close();
+        }
+    }
+
+    private async navigateToLoginPage(currentPage: Page) {
+        await currentPage.goto(
+            'https://sso.lifemiles.com/auth/realms/lifemiles/protocol/openid-connect/auth?client_id=lifemiles&response_type=code&scope=openid&login_hint=wst&ui_locales=en&redirect_uri=https://www.lifemiles.com/oauth-signin/&state=1712775357890&micrositeParams=%7B%27Access-Level%27%3A%20%270%27%2C%20%27Redirect-Uri%27%3A%20%27%27%7D',
+        );
+
+        // await Promise.all([
+        //     currentPage.waitForNavigation({ waitUntil: 'load' }),
+        //     currentPage.click(
+        //         '#FixedMenuHeader > div.menu-ui-Menu_headerRightWrapper > div > div > a',
+        //     ),
+        // ]);
+
+        await currentPage.waitForSelector('#social-Lifemiles', { visible: true });
+
+        await Promise.all([
+            currentPage.waitForNavigation({ waitUntil: 'load' }),
+            currentPage.click('#social-Lifemiles'),
+        ]);
+    }
+}
